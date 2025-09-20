273344f9c089f47c6638f2ce8e2bde3d9496b865
Emediong Francis <emediongfrancis@gmail.com>
2025-09-20 23:03:41 +0100

    Refactor variable assignments for clarity in simple interest calculation

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-principal, rate, time = 1000, 0.05, 3
+principal = 1000
+rate = 0.05
+time = 3
+
 interest = principal * rate * time
 
 print(f"The simple interest is: {interest}")
