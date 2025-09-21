c231fa60dd9d5af819c1165334b2fea83d16bd68
Emediong Francis <emediongfrancis@gmail.com>
2025-09-21 05:39:52 +0100

    Fix formatting of projected savings output to remove decimal places

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -6,4 +6,4 @@ total_expenses = int(input("Enter your total monthly expenses: "))
 savings = income - total_expenses
 print(f"Your monthly savings are ${savings}.")
 projected_savings = savings * 12 + (savings * 12 * 0.05)
-print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
